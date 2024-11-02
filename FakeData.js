184534fb8c9077ab902a369eb3176118b335766d
Oluwashina <shinzbaba@gmail.com>
2024-11-02 15:34:56 +0100

    resolvers usage with nodejs, graphql

--- FakeData.js
+++ FakeData.js
@@ -4,36 +4,108 @@ const UserList =  [
         name: 'John Doe',
         username: 'john_doe',
         age: 30,
-        nationality: 'American'
+        nationality: 'CANADA',
+        friends: [
+            {
+                id: 2,
+                name: 'Jane Smith',
+                username: 'jane_smith',
+                age: 28,
+                nationality: 'BRAZIL'
+            },
+            {
+                id: 3,
+                name: 'Alice Johnson',
+                username: 'alice_johnson',
+                age: 35,
+                nationality: 'INDIA'
+            }
+        ]
     },
     {
         id: 2,
         name: 'Jane Smith',
         username: 'jane_smith',
         age: 28,
-        nationality: 'British'
+        nationality: 'BRAZIL'
     },
     {
         id: 3,
         name: 'Alice Johnson',
         username: 'alice_johnson',
         age: 35,
-        nationality: 'American'
+        nationality: 'INDIA',
+        friends: [
+            {
+                id: 1,
+                name: 'John Doe',
+                username: 'john_doe',
+                age: 30,
+                nationality: 'CANADA'
+            }
+        ]
     },
     {
         id: 4,
         name: 'Bob Brown',
         username: 'bob_brown',
         age: 40,
-        nationality: 'British'
+        nationality: 'GERMANY'
     },
     {
         id: 5,
         name: 'Charlie Davis',
         username: 'charlie_davis',
         age: 25,
-        nationality: 'American'
+        nationality: 'CHILE'
     }
 ]
 
-module.exports = {UserList}
+
+const MovieList =  [
+    {
+        id: 1,
+        name: 'Inception',
+        yearOfPublication: 2010,
+        isInTheaters: true
+    },
+    {
+        id: 2,
+        name: 'The Matrix',
+        yearOfPublication: 1999,
+        isInTheaters: false
+    },
+    {
+        id: 3,
+        name: 'The Shawshank Redemption',
+        yearOfPublication: 1994,
+        isInTheaters: true
+    },
+    {
+        id: 4,
+        name: 'Star Wars: The Force Awakens',
+        yearOfPublication: 2015,
+        isInTheaters: true
+    },
+    {
+        id: 5,
+        name: 'The Dark Knight',
+        yearOfPublication: 2008,
+        isInTheaters: true
+    },
+    {
+        id: 6,
+        name: 'Avatar',
+        yearOfPublication: 2009,
+        isInTheaters: true
+    },
+    {
+        id: 7,
+        name: 'The Godfather',
+        yearOfPublication: 1972,
+        isInTheaters: true
+    }
+    
+]
+
+module.exports = {UserList, MovieList}
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,11 +1,37 @@
-const {UserList} = require('../FakeData')
+const {UserList, MovieList} = require('../FakeData')
+const _ = require("lodash")
 
 const resolvers = {
     Query : {
-        users() {
+
+        // USERS RESOLVERS
+        users: () => {
             return UserList
+        },
+        user: (parent, args) => {
+             const id = args.id
+             const user = _.find(UserList,{id : Number(id)})
+             return user || null
+        },
+
+        // MOVIES RESOLVERS
+        movies: () => {
+            return MovieList
+        },
+        movie : (parent, args) => {
+            const name = args.name
+            const movie = _.find(MovieList,{name})
+            return movie
         }
     },
+    User : {
+        favoriteMovies : () => {
+            return _.filter(
+                MovieList,
+                (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <=2010
+            )
+        }
+    }
  }
 
  module.exports = {resolvers}
