500eaa18ca1e0005be8d9710372003f260874a34
Oluwashina <shinzbaba@gmail.com>
2024-11-02 14:33:49 +0100

    Implementation of first query with graphql

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList =  [
+    {
+        id: 1,
+        name: 'John Doe',
+        username: 'john_doe',
+        age: 30,
+        nationality: 'American'
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        username: 'jane_smith',
+        age: 28,
+        nationality: 'British'
+    },
+    {
+        id: 3,
+        name: 'Alice Johnson',
+        username: 'alice_johnson',
+        age: 35,
+        nationality: 'American'
+    },
+    {
+        id: 4,
+        name: 'Bob Brown',
+        username: 'bob_brown',
+        age: 40,
+        nationality: 'British'
+    },
+    {
+        id: 5,
+        name: 'Charlie Davis',
+        username: 'charlie_davis',
+        age: 25,
+        nationality: 'American'
+    }
+]
+
+module.exports = {UserList}
